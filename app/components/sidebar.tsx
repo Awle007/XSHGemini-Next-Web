961f55620097f639c19a842bd20973f7455beeeb
Awle_ <141636413+Awle007@users.noreply.github.com>
2024-04-12 08:23:40 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          XSH云AI
+          XSH云OpenAi
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Powerd by XSH
+          Powerd by XSH云
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
