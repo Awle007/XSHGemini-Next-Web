d8b0d738690f4102196c6945401aa34e34a69bf3
Awle_ <141636413+Awle007@users.noreply.github.com>
2024-04-12 22:03:05 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          XSH云OpenAi
+          XSH云Gemini
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Powerd by XSH云
